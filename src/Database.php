1843a2f6b0ea4505c82c00e5ae33681b9b28b3f7
r_sunrise <rozasun61@gmail.com>
2026-01-13 13:42:23 +0700

    update database

--- src/Database.php
+++ src/Database.php
@@ -4,18 +4,18 @@ class Database {
     public static function getConnect() {
             try {
                 self::$pdo = new PDO(
-                    "pgsql:host=ep-lively-art-a1dka5rc-pooler.ap-southeast-1.aws.neon.tech;port=5432;dbname=neondb", 
+                    "pgsql:host=ep-quiet-shadow-a15z075v-pooler.ap-southeast-1.aws.neon.tech;dbname=neondb", 
                     "neondb_owner", 
-                    "npg_t4mxFrl2gCuW",         
+                    "npg_dTujtB6IxYb4",         
                     [
                         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
                     ]
                 );    
+        
             } catch (PDOException $e) {
                 die("Database connection failed: " . $e->getMessage());
             }
         return self::$pdo;
     }
 }
-
